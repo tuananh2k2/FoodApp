6714664c574ddf0bb9defd746d939b0901c2fe66
TNTA <anhtnt20@u.edu.vn>
2023-09-30 21:38:22 +0700

    Hello Thay

---
+++ app/src/main/java/vn/name/foodapp/activities/SplashActivity.java
@@ -0,0 +1,16 @@
+package vn.name.foodapp.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import vn.name.foodapp.R;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+    }
+}
